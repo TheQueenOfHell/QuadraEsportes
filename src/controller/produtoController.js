b1f5fe4d8a54641a5bd6a1ef65309fe4f189b562
Lidhia dos Santos <lidhiadossantos@gmail.com>
2021-01-21 15:42:36 -0300

    commiy aula5 incompleta

---
+++ src/controller/produtoController.js
@@ -0,0 +1,25 @@
+const Produto=require('../model/Produto')
+
+module.exports={
+    async list(req,res){
+
+    },
+    async filtro(req, res){
+
+    },
+    async abreadd(req,res){
+        res.render('')
+    },
+    async add(req,res){
+
+    },
+    async abreedit(req,res){
+
+    },
+    async edit(req,res){
+
+    },
+    async del(req,res){
+
+    }
+}
--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ const app=express();
 
 var porta=process.env.PORT || 3000;
 var session=require('express-session')
+var produtoRoute=require('./routes/produtoRoute')
+var quadraRoute=require('./routes/quadraRoute')
+var caixaRoute=require('./routes/caixaRoute')
 
 app.use(session({secret:'123',resave:true,saveUninitialized:true}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(flash())
 app.use(express.static(path.join("src","public")))
+app.use('/admin/produto',produtoRoute)
+app.use('/admin/quadra',quadraRoute)
+app.use('/caixa',caixaRoute)
 
 app.set('view engine', 'ejs')
 app.set('views', '.src/views')
